55342e77338c6df37cfc8d9ab56cb7b48112853c
Mathéo Morin <91942291+Matheoleharicot@users.noreply.github.com>
2025-11-03 11:50:51 +0100

    feat: add startGame function to manage game start logic and update key handling in Read_Key

--- TP/exercice6.c
+++ TP/exercice6.c
@@ -8,46 +8,64 @@
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
 OS_STK    task_key_stk[TASK_STACKSIZE];
+OS_STK    task_game_stk[TASK_STACKSIZE];
+
+#define TASK_KEY_PRIORITY 2
+#define TASK_GAME_PRIORITY 3
+
 
 void Read_SW(void* pdata);
+void startGame(void);
 
 // variavles partagées
 INT8U sw_value = 0;
 INT8U key_value = 0;
+
+void startGame(void){
+    if(sw_value == 1){
+        printf("Nouvelle partie commencée!\n");
+    } else if (sw_value == 2)
+    {
+        printf("Continuer la partie en cours.\n");
+    }
+}
 
 void Read_Key(void* pdata){
     while (1)
     {
         do {
             key_value = IORD_ALTERA_AVALON_PIO_DATA(KEY_BASE);
-        } while (key_value == 2);
+        } while (key_value == 3);
         
         printf("Bouton pressé: KEY[%d]\n", key_value);
         
         do {
             OSTimeDlyHMSM(0, 0, 0, 50);
-        } while (IORD_ALTERA_AVALON_PIO_DATA(KEY_BASE) != 0);
+        } while (IORD_ALTERA_AVALON_PIO_DATA(KEY_BASE) != 3);
     }
 }
+
 int main(void){
     OSInit();
     OSTaskCreateExt(Read_Key,
                     NULL,
                     (void *)&task_key_stk[TASK_STACKSIZE-1],
-                    2,
-                    2,
+                    TASK_KEY_PRIORITY,
+                    TASK_KEY_PRIORITY,
                     task_key_stk,
                     TASK_STACKSIZE,
                     NULL,
                     0);
-    
+    OSTaskCreateExt(startGame,
+                    NULL,
+                    (void *)&task_game_stk[TASK_STACKSIZE-1],
+                    TASK_GAME_PRIORITY,
+                    TASK_GAME_PRIORITY,
+                    task_game_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
     printf("Voulez-vous commencer une nouvelle partie ? ");
-    if(key_value == 1){
-        printf("Nouvelle partie commencée!\n");
-    } else if (key_value == 2)
-    {
-        printf("Continuer la partie en cours.\n");
-    }
     OSStart();
     return 0;
 }
