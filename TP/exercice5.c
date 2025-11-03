72f33933e4546bc52f34c5f03f1738f4febcf542
Mathéo Morin <91942291+Matheoleharicot@users.noreply.github.com>
2025-11-03 10:25:25 +0100

    feat: implement multitasking for timer and display functions
    - Added proper RTOS task management with separate tasks for time increment and display
    - Created shared timer_seconds variable to coordinate between tasks
    - Added seven segment display lookup table for proper digit rendering
    - Implemented proper task scheduling using OSTimeDlyHMSM for timing control
    - Defined distinct priority levels for increment (3) and display (4) tasks
    - Removed direct while loop in main and replaced with proper RTOS initialization and task

--- TP/exercice5.c
+++ TP/exercice5.c
@@ -8,93 +8,137 @@
 
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
-OS_STK    task1_stk[TASK_STACKSIZE];
+OS_STK    task_increment_stk[TASK_STACKSIZE];
+OS_STK    task_display_stk[TASK_STACKSIZE];
+OS_STK    task_temp_stk[TASK_STACKSIZE];
+
 #define TEMP_THRESHOLD 21
-#define TASK1_PRIORITY 1
+#define TASK_INCREMENT_PRIORITY 3
+#define TASK_DISPLAY_PRIORITY 4
+#define TASK_TEMP_PRIORITY 5
+
 INT8U err;
 alt_u8 celsius_lookup(int adc_avg_in);
-OS_EVENT *S1;
 void Temp_Alarm(void* pdata);
 void read_temperature ( void *pdata );
 void Increment_Time( void* pdata );
 void Display_Time(void* pdata);
-/* Definition of Task Stacks */
-#define   TASK_STACKSIZE       2048
 
+// Variable partagée pour le timer
+volatile INT32U timer_seconds = 0;
+
+// Table de conversion pour affichage 7 segments (0-9)
+const alt_u8 seven_seg_table[10] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F  // 9
+};
 
 alt_u8 celsius_lookup(int adc_avg_in)
 {
-	 const alt_u8 celsius_lookup_table[383]={
-			165,165,165,164,164,164,163,163,163,162,162,162,161,161,161,160,160,160,
-			159,159,159,158,158,158,157,157,157,156,156,156,155,155,155,154,154,154,
-			153,153,152,152,152,151,151,151,150,150,150,149,149,149,148,148,148,147,
-			147,147,146,146,146,145,145,144,144,144,143,143,143,142,142,142,141,141,
-			141,140,140,140,139,139,138,138,138,137,137,137,136,136,136,135,135,135,
-			134,134,133,133,133,132,132,132,131,131,131,130,130,129,129,128,128,128,
-			128,127,127,126,126,126,125,125,125,124,124,124,123,123,122,122,122,121,
-			121,121,120,120,119,119,119,118,118,118,117,117,116,116,116,115,115,114,
-			114,114,113,113,113,112,112,111,111,111,110,110,109,109,109,108,108,107,
-			107,107,106,106,105,105,105,104,104,103,103,103,102,102,101,101,101,100,
-			100,99,99,99,98,98,97,97,96,96,96,95,95,94,94,94,93,93,92,92,91,91,91,90,
-			 90,89,89,88,88,88,87,87,86,86,85,85,85,84,84,83,83,82,82,82,81,81,80,80,
-			 79,79,78,78,78,77,77,76,76,75,75,74,74,73,73,73,72,72,71,71,70,70,69,69,
-			 68,68,67,67,67,66,66,65,65,64,64,63,63,62,62,61,61,60,60,59,59,58,58,57,
-			 57,56,56,55,55,55,54,54,53,53,52,52,51,51,50,50,49,49,48,48,47,47,46,46,
-			 45,45,44,43,43,42,42,41,41,40,40,39,39,38,38,37,37,36,36,35,35,34,34,33,
-			 33,32,31,31,30,30,29,29,28,28,27,27,26,26,25,24,24,23,23,22,22,21,21,20,
-			 20,19,18,18,17,17,16,16,15,15,14,13,13,12,12,11,11,10, 9, 9, 8, 8, 7, 7,
-			  6, 5, 5, 4, 4, 3, 2, 2, 1, 1, 0 };
+    const alt_u8 celsius_lookup_table[383]={
+        165,165,165,164,164,164,163,163,163,162,162,162,161,161,161,160,160,160,
+        159,159,159,158,158,158,157,157,157,156,156,156,155,155,155,154,154,154,
+        153,153,152,152,152,151,151,151,150,150,150,149,149,149,148,148,148,147,
+        147,147,146,146,146,145,145,144,144,144,143,143,143,142,142,142,141,141,
+        141,140,140,140,139,139,138,138,138,137,137,137,136,136,136,135,135,135,
+        134,134,133,133,133,132,132,132,131,131,131,130,130,129,129,128,128,128,
+        128,127,127,126,126,126,125,125,125,124,124,124,123,123,122,122,122,121,
+        121,121,120,120,119,119,119,118,118,118,117,117,116,116,116,115,115,114,
+        114,114,113,113,113,112,112,111,111,111,110,110,109,109,109,108,108,107,
+        107,107,106,106,105,105,105,104,104,103,103,103,102,102,101,101,101,100,
+        100,99,99,99,98,98,97,97,96,96,96,95,95,94,94,94,93,93,92,92,91,91,91,90,
+        90,89,89,88,88,88,87,87,86,86,85,85,85,84,84,83,83,82,82,82,81,81,80,80,
+        79,79,78,78,78,77,77,76,76,75,75,74,74,73,73,73,72,72,71,71,70,70,69,69,
+        68,68,67,67,67,66,66,65,65,64,64,63,63,62,62,61,61,60,60,59,59,58,58,57,
+        57,56,56,55,55,55,54,54,53,53,52,52,51,51,50,50,49,49,48,48,47,47,46,46,
+        45,45,44,43,43,42,42,41,41,40,40,39,39,38,38,37,37,36,36,35,35,34,34,33,
+        33,32,31,31,30,30,29,29,28,28,27,27,26,26,25,24,24,23,23,22,22,21,21,20,
+        20,19,18,18,17,17,16,16,15,15,14,13,13,12,12,11,11,10, 9, 9, 8, 8, 7, 7,
+        6, 5, 5, 4, 4, 3, 2, 2, 1, 1, 0 };
 
-	return (celsius_lookup_table[adc_avg_in]);
+    return (celsius_lookup_table[adc_avg_in]);
 }
 
 void read_temperature ( void *pdata ){
-	int adc_value = 0;
-	for (int i = 0; i < 64; i++)
-	{
-		adc_value += IORD(MODULAR_ADC_0_SAMPLE_STORE_CSR_BASE, i);
-	}
-	adc_value = adc_value / 64 - 3416;
-	alt_u8 temperature = celsius_lookup(adc_value) - 40;
-	printf("Temperature: %d C\n", temperature);
+    int adc_value = 0;
+    for (int i = 0; i < 64; i++)
+    {
+        adc_value += IORD(MODULAR_ADC_0_SAMPLE_STORE_CSR_BASE, i);
+    }
+    adc_value = adc_value / 64 - 3416;
+    alt_u8 temperature = celsius_lookup(adc_value) - 40;
+    printf("Temperature: %d C\n", temperature);
 }
 
 void Temp_Alarm(void* pdata) {
-	alt_u8 temperature;
-	read_temperature(temperature);
-	if (temperature > TEMP_THRESHOLD) {
-		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, (0x1));
-		printf("! Temperature exceeded threshold: %d C !\n", temperature);
-	}
-	else {
-		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, (0x0));
-	}
-}
-void Increment_Time( void* pdata ) {
-	int* seconds = (int*)pdata;
-    while (1)
-    {
-        usleep(1000000);
-        (*seconds)++;
-        printf("Temps écoulé : %u secondes\n", *seconds);
+    alt_u8 temperature;
+    read_temperature(temperature);
+    if (temperature > TEMP_THRESHOLD) {
+        IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, (0x1));
+        printf("! Temperature exceeded threshold: %d C !\n", temperature);
+    }
+    else {
+        IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, (0x0));
     }
 }
-void Display_Time(void* pdata){
-	INT8U value = *((int*)pdata);
-	IOWR_ALTERA_AVALON_PIO_DATA(SEVEN_SEG_0_BASE ,value);
+
+void Increment_Time( void* pdata ) {
+    while (1)
+    {
+        OSTimeDlyHMSM(0, 0, 1, 0);
+        timer_seconds++;
+        printf("Temps écoulé : %u secondes\n", timer_seconds);
+    }
 }
+
+void Display_Time(void* pdata){
+    while (1)
+    {
+        INT32U seconds = timer_seconds % 10;
+        alt_u8 display_value = seven_seg_table[seconds];
+        
+        IOWR_ALTERA_AVALON_PIO_DATA(SEVEN_SEG_0_BASE, display_value);
+        
+        OSTimeDlyHMSM(0, 0, 0, 100);
+    }
+}
+
 int main(void){
-	IOWR(MODULAR_ADC_0_SEQUENCER_CSR_BASE, 0, 0);
-	usleep(10);
-	IOWR(MODULAR_ADC_0_SAMPLE_STORE_CSR_BASE, 64, 0);
-	IOWR(MODULAR_ADC_0_SEQUENCER_CSR_BASE, 0, 1);
-	int timer = 0;
-	while (1)
-	{
-		Increment_Time(timer);
-		Display_Time(&timer);
-		read_temperature(NULL);
-	}
-	
-	return 0;
+    IOWR(MODULAR_ADC_0_SEQUENCER_CSR_BASE, 0, 0);
+    usleep(10);
+    IOWR(MODULAR_ADC_0_SAMPLE_STORE_CSR_BASE, 64, 0);
+    IOWR(MODULAR_ADC_0_SEQUENCER_CSR_BASE, 0, 1);
+    
+    OSInit();
+    
+    OSTaskCreateExt(Increment_Time,
+                    NULL,
+                    (void *)&task_increment_stk[TASK_STACKSIZE-1],
+                    TASK_INCREMENT_PRIORITY,
+                    TASK_INCREMENT_PRIORITY,
+                    task_increment_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
+    
+    OSTaskCreateExt(Display_Time,
+                    NULL,
+                    (void *)&task_display_stk[TASK_STACKSIZE-1],
+                    TASK_DISPLAY_PRIORITY,
+                    TASK_DISPLAY_PRIORITY,
+                    task_display_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
+    OSStart();
+    
+    return 0;
 }
